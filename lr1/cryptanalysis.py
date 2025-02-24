dcd980850f3ed030d55efbbbd629b2eb854d443b
stugnait <i.teslitskii@gmail.com>
2025-02-24 23:18:31 +0200

    lr1 fix

--- lr1/cryptanalysis.py
+++ lr1/cryptanalysis.py
@@ -7,8 +7,19 @@ def frequency_analysis(text):
     frequency = {char: count / total_letters for char, count in letter_counts.items()}
     return frequency
 
+def decrypt(text, shift):
+    decrypted_text = []
+    for char in text:
+        if char.isalpha():
+            new_char = chr(((ord(char.lower()) - ord('a') - shift) % 26) + ord('a'))
+            decrypted_text.append(new_char)
+        else:
+            decrypted_text.append(char)
+    return ''.join(decrypted_text)
+
+
 # Зчитування зашифрованого тексту з файлу
-with open('encrypted_text.txt', 'r', encoding='utf-8') as file:
+with open('resources/encrypted_text.txt', 'r', encoding='utf-8') as file:
     encrypted_text = file.read()
 
 # Виконання частотного аналізу
@@ -20,7 +31,10 @@ for char, freq in sorted(frequencies.items(), key=lambda x: x[1], reverse=True):
     print(f"{char}: {freq:.4f}")
 
 # Пошук ймовірного зсуву на основі частоти літери
-# Припустимо, що найчастіша літера у зашифрованому тексті відповідає 'о' (для української мови)
 most_frequent_char = max(frequencies, key=frequencies.get)
-shift = (ord(most_frequent_char) - ord('о')) % 26
+shift = (ord(most_frequent_char) - ord('e')) % 26
 print(f"Ймовірний зсув: {shift}")
+
+decrypted_text = decrypt(encrypted_text, shift)
+print("Розшифроване слово/текст:")
+print(decrypted_text)
